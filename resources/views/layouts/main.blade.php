ee4a2e8d9a79641dfb7490ee0ead719294cc2dca
fauzi abdillah <fauziabd26>
2022-04-06 13:59:39 +0700

    CRUD Barang

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <link rel="icon" href="{{asset('stisla')}}/img/polindra.png" type="image" sizes="16x16">
-  <title>SIPER &mdash; POLINDRA</title>
+  <title>SILK &mdash; POLINDRA</title>
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
