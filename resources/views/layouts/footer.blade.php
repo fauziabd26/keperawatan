9f45c5fc291ce7cc9f44840d4dfbf822eb0bd9e0
Fauzi Abdilah <59874453+fauziabd26@users.noreply.github.com>
2022-06-01 16:32:02 +0700

    delte recources

--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-        <div class="footer-left">
-          Copyright &copy; 2022 <div class="bullet"></div><a href="https://polindra.ac.id">POLITEKNIK NEGERI INDRAMAYU</a>
-        </div>
-        <div class="footer-right">
-          2.3.0
-        </div>
-      </footer>
